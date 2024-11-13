f3b84a2c0004ec79cbfe1963eb810a6ba8343fdb
BugFreeGuy <tsamaiphenyo@gmail.com>
2024-11-13 20:01:51 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Add JavaScript code for functionality and interactions
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  console.log('Form submitted!');
+  // Add form submission logic here
+});
+feather.replace()
